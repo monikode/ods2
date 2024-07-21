54d31da87f76f8c4d5801aad33c9731e68f0fc73
Luana Martins Dutra <99104871+LuanaMartinsDutra@users.noreply.github.com>
2024-07-21 17:13:20 -0400

    v1
    saldo inicial, realiza deposito e valor saldo+deposito

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    networks: {
-      development: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*",
-      },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,  // Certifique-se de que a porta corresponde à do Ganache CLI
+      network_id: "*", // Match any network id
     },
-    compilers: {
-      solc: {
-        version: "0.8.0",
-      },
+  },
+  compilers: { 
+    solc: {
+      version: "0.8.0", // Specify compiler version
     },
-  };
-  
+  },
+};
